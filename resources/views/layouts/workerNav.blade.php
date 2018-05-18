f54e7c690142ff8560a0c7cae1a91b2dcd44d3dd
Jezeh Priesten <jepriesten@gmail.com>
2018-05-18 20:19:21 +0100

    Included the logo and favicon

--- resources/views/layouts/workerNav.blade.php
+++ resources/views/layouts/workerNav.blade.php
@@ -1,42 +1,46 @@
  <!-- Navigation -->
- <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation" id="navv">
+ <nav class="navbar navbar-default navbar-static-top topnav">
         
         <div class="container">
             
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <button type="button" class="navbar-toggle collapse" data-toggle="collapse" data-target="#position" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                    
                 </button>
-                <a class="navbar-brand topnav" href="/" > SmallBoulo </a>
+                <a class="navbar-brand" href="/">
+                    <img alt="Brand" src="/pics/logo.jpeg" width=70px height=70px>
+                </a>
             </div>
             
 
             <!-- Collect the nav links, forms, and other content for toggling ,go-to structure -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
+            <div class="collaps navbar-collaps" id="position" >
                 <ul class="nav navbar-nav navbar-right">
                     
-                    <li>
+                    <!-- <li>
                         <a id="signupL" href="/sign_up">Sign Up</a>
                     </li>
                     <li>
                         <a  id="loginL" href="/log_in">Log In<span class="glyphicon glyphicon-log-in"></span></a>
-                    </li>
+                    </li> -->
                     <li>
                         <a href="/worker" id="workerSearch">Search<span class="glyphicon glyphicon-search"></span></a>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a href="/worker/profile" id="workerProfile">My Profile<span class="glyphicon glyphicon-user"></span></a>
-                    </li>
-                    <li>
+                    </li> -->
+                    <!-- <li>
                         <a id="logoutBtn" href="/log_out" class="LogOut">Log Out<span class="glyphicon glyphicon-log-out"></span></a>
-                    </li>
+                    </li> -->
                 </ul>
             </div> <!-- .navbar-collapse -->   
         </div><!-- /.container -->
-    </nav><!-- naivagtion -->
+    </nav> <!-- naivagtion -->
+
+    
 
     
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
     <!-- write functional code to dynamically change the navbar according to the view -->
     @if('worker' == 'worker')
 
-        @include('layouts.WorkerNav')
+        @include('layouts.nav')
 
     @elseif('employer' == 'employer')
 
@@ -14,154 +14,93 @@
 
  @section('content')
 
-
-    <!-- header -->
-    <a name="search"></a>
-
-    <div class="intro-header">
-        <div class="container-fluid" >
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-lg-6"  id="employerCol">
-
-                    <br>
-                    <br>
-                    <h2 style="color:green"> GET WORKERS FOR YOUR JOBS FASTER THAN NEVER BEFORE <h1>
-                        <hr>
-                        <h2> post a <strong>SmallBoulo</strong> for workers around your location </h2>
-                        <br>
-                        <br>
-
-
-                       <a href="/employer"><button class="btn btn-success btn-lg" id="employerBtn" > EMPLOYER </button></a>
-
-
-                       <h3> get workers    </h3>
-                </div>
-
-                <div class="col-md-6 col-sm-6 col-lg-6" id="workerCol">
-
-                    <br>
-                    <br>
-                    <h2 style="color:#db7b1e"> GET EMPLOYED FOR A SMALLBOULO FASTER THAN NEVER BEFORE <h1>
-                        <hr>
-                        <h2> check for any <strong>SmallBoulo</strong> posted around your location </h2>
-                        <br>
-                        <br>
-                       
-
-                        <a href="/worker"><button class="btn btn-warning btn-lg" id="workerBtn"> WORKER </button></a>
-
-                        <h3> search SmallBoulo </h3>
-                </div>
-            </div>  
-        </div>
-        <!-- /.container -->
-    </div> 
-    <!-- /.intro-header end -->
+    <div id="bodyPosition">
         
+         <!-- header -->
+        <a name="search"></a>
 
-    <!-- about smallboulo  -->
+        <div class="intro-header">
+            <div class="container-fluid" >
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-lg-6"  id="employerCol">
 
-    <a  name="about"></a>
-    <!-- content-section a -->
-    <div class="content-section-a">
-       <div class="container">
-            <div class="row">
-
-                <div class="col-md-4"  id="rightAlign">
-                    <hr class="section-heading-spacer-right" id="hr">
-                    <div class="clearfix"></div>                
-                    <h2 class="section-emp-heading">are you an Employer ? <br><div id="ans"> Hire workers close to you <div></h2>
-                    <p class="lead"> you need workers,connect and post a smallBoulo offer,when you get applicants, views thier profiles and hire one of them </p>
-                    
-                </div>
-
-                <div class="clearfix visible-xs"></div> <!-- for mobile display -->  
-
-                <div class="col-md-4"  >
-                    <br>
-                    <img class="img-responsive rounded" src="/pics/sb.jpg" alt=" SmallBoulo" id="logo">
-                </div>
-
-                <div class="clearfix visible-xs"></div> <!-- for mobile display --> 
-
-                <div class="col-md-4">
-                    <hr class="section-heading-spacer-left" id="hr">
-                    <div class="clearfix"></div>                
-                    <h2 class="section-wo-heading">are you a Worker ?<br><div id="ans"> There are jobs for you <div></h2>
-                    <p class="lead"> Have the opportunity to apply for posted SmallBoulos around your residence, no more stress to have a deal with an employer,we bring the closet jobs to you</p>
-                </div>
-
-            </div> <!-- row end -->
-        </div> <!-- /.container -->
-     </div> <!-- section-a end -->
+                        <br>
+                        <br>
+                        <h2 style="color:green"> GET WORKERS FOR YOUR JOBS FASTER THAN NEVER BEFORE <h1>
+                            <hr>
+                            <h2> post a <strong>SmallBoulo</strong> for workers around your location </h2>
+                            <br>
+                            <br>
 
 
-    <!-- Modal -->
-    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModallabel" aria-hidden="true">
-        <div class="modal-dialog"> Log Into SmallBoulo
-            <div class="modal-content">
-                
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times;</button>
-                    <h4 class="modal-title employerWorkerC"> Log In </h4>
-                </div> 
+                        <a href="/employer"><button class="btn btn-success btn-lg" id="employerBtn" > EMPLOYER </button></a>
 
-                <div class="modal-body">
+
+                        <h3> get workers    </h3>
+                    </div>
+
+                    <div class="col-md-6 col-sm-6 col-lg-6" id="workerCol">
+
+                        <br>
+                        <br>
+                        <h2 style="color:#db7b1e"> GET EMPLOYED FOR A SMALLBOULO FASTER THAN NEVER BEFORE <h1>
+                            <hr>
+                            <h2> check for any <strong>SmallBoulo</strong> posted around your location </h2>
+                            <br>
+                            <br>
                         
-                        <div class="form-horizontal">
-                            <fieldset>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-3 col-sm-4">
-                                        <label class="checkbox-inline workerColor">
-                                           <input type="radio" name="selectedWF" value="worker" checked> Worker 
-                                        </label>
-                                    </div>
-                                    <div class="col-sm-5">
-                                        <label class="checkbox-inline employerColor">
-                                           <input type="radio" name="selectedWF" value="employer" checked> Employer 
-                                        </label>   
-                                    </div>
-                                </div>
-                            </fieldset>
-                            <fieldset>
-                                <legend id="employerWorkerC">SmallBoulo Login</legend>
-                                <p id="loginError"></p><p id="loginSuccess"></p>
-                                <div class="form-group">
-                                  <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
-                                  <div class="col-sm-8">
-                                    <input type="email" name="empEmail" class="form-control" id="loginEmail" placeholder="john.doe@domain.com" required>
-                                  </div>
-                                </div>
-                                <div class="form-group">
-                                  <label for="inputPassword3" class="col-sm-3 control-label">Password</label>
-                                  <div class="col-sm-8">
-                                    <input type="password" name="empPassword" class="form-control" id="loginPassword" placeholder="Password" required>
-                                  </div>
-                                </div>
-                                <div class="col-sm-offset-3 col-sm-8">
-                                  <a id="forgotPassword" href="#">Forgot your Password</a>
-                                </div>
-                                <div class="form-group">
-                                  <div class="col-sm-offset-3 col-sm-8">
-                                    <div class="checkbox">
-                                      <label>
-                                        <input type="checkbox"> Remember me
-                                      </label>
-                                    </div>
-                                  </div>
-                                </div>
-                            </fieldset>
-                        </div>
-                </div>
 
-                <div class="modal-footer">
-                    <button class="btn btn-danger pull-left" data-dismiss="modal"> Cancel </button>
-                    <button id="EWLoginBtn" class="btn btn-primary"> Log In </button>
-                </div>
-            </div><!-- /.modal-content -->
-        </div>
-   </div><!-- /.log in modal end -->
+                            <a href="/worker"><button class="btn btn-warning btn-lg" id="workerBtn"> WORKER </button></a>
+
+                            <h3> search SmallBoulo </h3>
+                    </div>
+                </div>  
+            </div>
+            <!-- /.container -->
+        </div> 
+        <!-- /.intro-header end -->
+            
+
+        <!-- about smallboulo  -->
+
+        <a  name="about"></a>
+        <!-- content-section a -->
+        <div class="content-section-a">
+        <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4"  id="rightAlign">
+                        <hr class="section-heading-spacer-right" id="hr">
+                        <div class="clearfix"></div>                
+                        <h2 class="section-emp-heading">are you an Employer ? <br><div id="ans"> Hire workers close to you <div></h2>
+                        <p class="lead"> you need workers,connect and post a smallBoulo offer,when you get applicants, views thier profiles and hire one of them </p>
+                        
+                    </div>
+
+                    <div class="clearfix visible-xs"></div> <!-- for mobile display -->  
+
+                    <div class="col-md-4"  >
+                        <br>
+                        <img class="img-responsive rounded" src="/pics/sb.jpg" alt=" SmallBoulo" id="logo">
+                    </div>
+
+                    <div class="clearfix visible-xs"></div> <!-- for mobile display --> 
+
+                    <div class="col-md-4">
+                        <hr class="section-heading-spacer-left" id="hr">
+                        <div class="clearfix"></div>                
+                        <h2 class="section-wo-heading">are you a Worker ?<br><div id="ans"> There are jobs for you <div></h2>
+                        <p class="lead"> Have the opportunity to apply for posted SmallBoulos around your residence, no more stress to have a deal with an employer,we bring the closet jobs to you</p>
+                    </div>
+
+                </div> <!-- row end -->
+            </div> <!-- /.container -->
+        </div> <!-- section-a end -->
+
+    </div>
+
+
+
 
 
    
